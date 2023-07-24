fbafc8e178a645a565cfd41c5d3ffb530ab3d83d
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-07-24 13:25:31 +0300

    Hometask 3  completed

---
+++ Hometask 003/app.js
@@ -0,0 +1,4 @@
+const userHours = prompt('Please, enter hours: ')
+const userHoursInSeconds = userHours * 3600
+alert(`There is ${userHoursInSeconds} seconds in ${userHours} hours.`)
+console.log(`There is ${userHoursInSeconds} seconds in ${userHours} hours.`)
