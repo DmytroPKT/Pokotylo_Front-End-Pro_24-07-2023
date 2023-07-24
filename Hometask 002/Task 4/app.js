0441197ec2d4fc2eb7f3d4c66b2259f9536e62d7
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-07-24 13:01:13 +0300

    Hometask 2 - 4th task completed

---
+++ Hometask 002/Task 4/app.js
@@ -0,0 +1,5 @@
+const firstNumber = Number(prompt("Enter first number: "))
+const secondNumber = Number(prompt("Enter second number: "))
+const thirdNumber = Number(prompt("Enter third number: "))
+let result = (firstNumber + secondNumber + thirdNumber) / 3
+alert(`Arithmetic mean for ${firstNumber}, ${secondNumber} and ${thirdNumber} is ${result}`)
