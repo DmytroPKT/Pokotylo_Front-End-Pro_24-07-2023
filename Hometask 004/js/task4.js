6f1ce4804fc4b7101eb82257bd4f8700b67ea323
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-07-25 15:00:21 +0300

    Task 4 modified

--- Hometask 004/js/task4.js
+++ Hometask 004/js/task4.js
@@ -3,18 +3,18 @@
 let firstNumber = parseFloat(prompt('Enter first number'))
 let secondNumber = parseFloat(prompt('Enter second number'))
 
-let firstResult = firstNumber % 10
-let secondResult = secondNumber % 10
+let firstNumberLastDigit = firstNumber % 10
+let secondNumberLastDigit = secondNumber % 10
 
 
 if (firstNumber % 2 === 0) {
-    console.log(`First number is pair, last digit of ${firstNumber} is ${firstResult}`)
+    console.log(`First number is pair, last digit of ${firstNumber} is ${firstNumberLastDigit}`)
 } else {
-    console.log(`First number is not pair,  last digit of ${firstNumber} is ${firstResult}`)
+    console.log(`First number is not pair,  last digit of ${firstNumber} is ${firstNumberLastDigit}`)
 }
 
 if (secondNumber % 2 === 0) {
-    console.log(`Second number is a pair, last digit of ${secondNumber} is ${secondResult}`)
+    console.log(`Second number is a pair, last digit of ${secondNumber} is ${secondNumberLastDigit}`)
 } else {
-    console.log(`Second number is not pair, , last digit of ${secondNumber} is ${secondResult}`)
+    console.log(`Second number is not pair, , last digit of ${secondNumber} is ${secondNumberLastDigit}`)
 }
