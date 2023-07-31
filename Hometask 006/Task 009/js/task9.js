b069cad11f7eafbd3096912702c7ff0c566a1c6d
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-07-31 18:25:21 +0300

    Hometask 6.9  modified

--- Hometask 006/Task 009/js/task9.js
+++ Hometask 006/Task 009/js/task9.js
@@ -2,37 +2,39 @@ document.write(`Hometask 6 - 9th task. Дано натуральне число.
 a.Визначити кількість його парних дільників;
 b.Знайти суму його парних дільників.`);
 
-const userNumber = parseInt(prompt('Please enter natural number: '));
+let userNumber 
 let numberDivisors = '';
 let evenNumberDivisors = [];
 let evenNumberDivisorsSum = 0;
 
-if (!userNumber || userNumber < 0){
-    alert('Invalid input!');
-} else {
-    for (let i = 1; i <= userNumber; i++) {
-        if (userNumber % i != 0) {
-            continue;
-        }
-        if (i % 2 === 0) {
-            evenNumberDivisors.push(i);
-            evenNumberDivisorsSum += i;
-        }
-        if (userNumber === i) {
-            numberDivisors += `${i}`;
-            break;
-        }
-        numberDivisors += `${i}, `;
+do {
+    if (userNumber === null && !(isNaN(userNumber))) {
+        break;
     }
-    if (evenNumberDivisorsSum === 0) {
-        console.log(`The number ${userNumber} can be divided by: ${numberDivisors};
-        \n\ta.No any even divisors for the number ${userNumber}!;
-        \n\tb.No any sum of even divisors for the number ${userNumber}!`);
-    } else {
-        console.log(`The number ${userNumber} can be divided by: ${numberDivisors};
-        \n\ta.The even divisors of the number ${userNumber} are: ${evenNumberDivisors}. Their count is ${evenNumberDivisors.length};
-        \n\tb.The sum of even divisors of the number ${userNumber} is ${evenNumberDivisorsSum}.`);
-    }
-}
-
-
+    userNumber = parseInt(prompt('Please enter natural number: '));
+    if (!isNaN(userNumber) && (1 <= userNumber)) {
+        for (let i = 1; i <= userNumber; i++) {
+            if (userNumber % i != 0) {
+                continue;
+            };
+            if (i % 2 === 0) {
+                evenNumberDivisors.push(i);
+                evenNumberDivisorsSum += i;
+            };
+            if (userNumber === i) {
+                numberDivisors += `${i}`;
+                break;
+            };
+            numberDivisors += `${i}, `;
+        };
+        if (evenNumberDivisorsSum === 0) {
+            console.log(`The number ${userNumber} can be divided by: ${numberDivisors};
+            \n\ta.No any even divisors for the number ${userNumber}!;
+            \n\tb.No any sum of even divisors for the number ${userNumber}!`);
+        } else {
+            console.log(`The number ${userNumber} can be divided by: ${numberDivisors};
+            \n\ta.The even divisors of the number ${userNumber} are: ${evenNumberDivisors}. Their count is ${evenNumberDivisors.length};
+            \n\tb.The sum of even divisors of the number ${userNumber} is ${evenNumberDivisorsSum}.`);
+        };
+    };
+} while(isNaN(userNumber) || (userNumber < 1));
