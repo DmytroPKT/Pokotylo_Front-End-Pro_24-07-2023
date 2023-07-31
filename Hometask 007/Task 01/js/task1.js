700a6dcc7b7a612a301047ca5169c7e850ceb5d9
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-07-31 16:58:04 +0300

    Hometask 7.1  completed

---
+++ Hometask 007/Task 01/js/task1.js
@@ -0,0 +1,11 @@
+document.write('Hometask 7 - 1st task. Вивести числа від 20 до 30 через пропуск використовуючи крок 0,5 (20 20,5 21 21,5….)');
+
+let start = 20;
+const end = 30;
+let result = '';
+
+while(start <= end) {
+    result += `${start} `;
+    start = start + 0.5;
+}
+console.log(result)
