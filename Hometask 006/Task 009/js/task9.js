3381b2e6d96bd1df21d00851949b4c4f1ebf6cdf
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-07-31 16:28:21 +0300

    Hometask 6 - 9 task completed

--- Hometask 006/Task 009/js/task9.js
+++ Hometask 006/Task 009/js/task9.js
@@ -1,17 +1,27 @@
-document.write('Hometask 6 - 9th task. Дано натуральне число. Знайти та вивести на сторінку всі його дільники.');
+document.write(`Hometask 6 - 9th task. Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
+a.Визначити кількість його парних дільників;
+b.Знайти суму його парних дільників.`);
 
 const userNumber = parseInt(prompt('Please enter natural number: '));
-let result = '';
+let numberDivisors = '';
+let evenNumberDivisors = [];
+let evenNumberDivisorsSum = 0;
 
 for (let i = 1; i <= userNumber; i++) {
     if (userNumber % i != 0) {
         continue;
-    } else if (userNumber === i) {
-        result += `${i}.`;
+    }
+    if (i % 2 === 0) {
+        evenNumberDivisors.push(i);
+        evenNumberDivisorsSum += i;
+    }
+    if (userNumber === i) {
+        numberDivisors += `${i}`;
         break;
     }
-    
-    result += `${i}, `;
+    numberDivisors += `${i}, `;
 }
-console.log(`The number ${userNumber} can be divided by: ${result}`);
 
+console.log(`The number ${userNumber} can be divided by: ${numberDivisors};
+\n\ta.The even divisors of the number ${userNumber} are: ${evenNumberDivisors}. Their count is ${evenNumberDivisors.length};
+\n\tb.The sum of even divisors of the number ${userNumber} is ${evenNumberDivisorsSum}.`);
