3467f9d12b4a6acd153b52b22b8a3ce708f88bcb
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-08-10 21:27:54 +0300

    ht 10 fixed

--- Hometask 010/js/functions.js
+++ Hometask 010/js/functions.js
@@ -1,49 +1,26 @@
-function firstMessage(productsAmount) {
+function getCategoriesList () {
+    let message = '';
+    for (let i = 1; i <= productsArray.length; i++) {
+        message += `No${i} - ${productsArray[i-1]}\n`;
+    }
+    return message;
+} 
+
+function firstMessage() {
     let category;
     let confirmMessage;
     let requirements;
     do {
         do {
-            category = parseInt(prompt('Please, enter category number:\n1 - phones,\n2 - laptops,\n3 - monitors,\n4 - printers,\n5 - routers.'));
-            requirements = (category > productsAmount) || (category < 0) || !category || isNaN(category);
+            category = parseInt(prompt(`Please, enter category number:\n${categoryList}.`));
+            requirements = (category > productsArray.length) || (category < 0) || !category || isNaN(category);
             if (requirements) {
-                alert(`Invalid input! Please enter only digits from 1 to ${productsAmount}!`);
+                alert(`Invalid input! Please enter only digits from 1 to ${productsArray.length}!`);
             };
         } while(requirements);
-        confirmMessage = confirm(`Are you sure that you wanna choose ${inputConvertLabel(category)}?`);
+        confirmMessage = confirm(`Are you sure that you wanna choose ${productsArray[category - 1]}?`);
     } while(!confirmMessage);
     return category;
-}
-
-function inputConvertLabel (input) {
-    let label;
-    switch(input){
-        case 1:
-            return label = 'phones';
-        case 2:
-            return label = 'laptops';
-        case 3:
-            return label = 'monitors';
-        case 4:
-            return label = 'printers';
-        case 5:
-            return label = 'routers';
-    };
-}
-
-function inputConvert (input) {
-    switch(input){
-        case 1:
-            return input = products.phones;
-        case 2:
-            return input = products.laptops;
-        case 3:
-            return input = products.monitors;
-        case 4:
-            return input = products.printers;
-        case 5:
-            return input = products.routers;
-    };
 }
 
 function showProducts(category) {
@@ -53,52 +30,52 @@ function showProducts(category) {
     };
 }
 
-function productNo (cat, label) {
+function getProductNumber () {
     let productNumber;
     let confirmMessage;
     let requirements;
     do {
         do {
-            productNumber = parseInt(prompt(`Please, choose any number of ${label}:`));
-            requirements = (productNumber < 1) || (productNumber > cat.length) || isNaN(productNumber);
+            productNumber = parseInt(prompt(`Please, choose any number of ${customerCategoryName}:`));
+            requirements = (productNumber < 1) || (productNumber > customerCategory.length) || isNaN(productNumber);
             if (requirements) {
-                alert(`We have only ${cat.length} products in category of ${label}. There is no any product with a number ${productNumber}! Try again.`);
+                alert(`We have only ${customerCategory.length} products in category of ${customerCategoryName}. There is no any product with a number ${productNumber}! Try again.`);
             };
         } while(requirements);
-        confirmMessage = confirm(`Are you sure that you wanna choose ${cat[productNumber - 1].brand} ${cat[productNumber - 1].series} ${cat[productNumber - 1].model}?`);
+        confirmMessage = confirm(`Are you sure that you wanna choose ${customerCategory[productNumber - 1].brand} ${customerCategory[productNumber - 1].series} ${customerCategory[productNumber - 1].model}?`);
     } while(!confirmMessage);
     return productNumber - 1;
 }
 
-function getAmount(productNu, label) {
+function getAmount() {
     let quantity;
     let confirmMessage;
     let requirements;
     do {
         do {
-            quantity = parseInt(prompt(`How many ${productNu.brand} ${productNu.series} ${productNu.model} ${label} for ${productNu.price} UAH you wanna to buy?`));
-            requirements = (quantity < 1) || (quantity > productNu.inStore) || isNaN(quantity);
-            if (quantity > productNu.inStore) {
-                alert(`${productNu.brand} ${productNu.series} ${productNu.model} remain in store only ${productNu.inStore} pc(s)!`);
+            quantity = parseInt(prompt(`How many ${orderNumber.brand} ${orderNumber.series} ${orderNumber.model} ${customerCategoryName} for ${orderNumber.price} UAH you wanna to buy?`));
+            requirements = (quantity < 1) || (quantity > orderNumber.inStore) || isNaN(quantity);
+            if (quantity > orderNumber.inStore) {
+                alert(`${orderNumber.brand} ${orderNumber.series} ${orderNumber.model} remain in store only ${orderNumber.inStore} pc(s)!`);
             } else if (requirements) {
                 alert('Invalid input! You have to enter positive natural numbers only!');
             };
         } while(requirements);
-        confirmMessage = confirm(`Are you sure that you wanna buy ${productNu.brand} ${productNu.series} ${productNu.model} in amount of ${quantity} pc(s)?`);
+        confirmMessage = confirm(`Are you sure that you wanna buy ${orderNumber.brand} ${orderNumber.series} ${orderNumber.model} in amount of ${quantity} pc(s)?`);
     } while(!confirmMessage);
     return quantity;
 }
 
-function discountPrice (priceOrder, productNu, qty, discCoef, dicMessage) {
-    let finalDiscountPrice = priceOrder * discCoef;
-    console.log(`${dicMessage} ${priceOrder} UAH, yours order price for ${productNu.brand} ${productNu.series} ${productNu.model} in amount of ${qty} pc(s) will be just only ${finalDiscountPrice.toFixed(2)} UAH!`);
+function discountPrice (price) {
+    let finalDiscountPrice = price * discountCoefficient;
+    console.log(`${discountMessage} ${price} UAH, yours order price for ${orderNumber.brand} ${orderNumber.series} ${orderNumber.model} in amount of ${orderQuantity} pc(s) will be just only ${finalDiscountPrice.toFixed(2)} UAH!`);
 }
 
-function priceFunc(productNu, qty, discStart, discCoef, dicMessage) {
-    let orderPrice = productNu.price * qty;
-    if (orderPrice > discStart) {
-        discountPrice(orderPrice, productNu, qty, discCoef, dicMessage);
+function priceFunc() {
+    let orderPrice = orderNumber.price * orderQuantity;
+    if (orderPrice > discountStart) {
+        discountPrice(orderPrice);
     } else {
-        console.log(`Yours order price for ${productNu.brand} ${productNu.model} in amount of ${qty} pc(s) will be ${orderPrice} UAH.`);
+        console.log(`Yours order price for ${orderNumber.brand} ${orderNumber.model} in amount of ${orderQuantity} pc(s) will be ${orderPrice} UAH.`);
     };
 }
--- Hometask 010/js/main.js
+++ Hometask 010/js/main.js
@@ -1,20 +1,18 @@
 const discount = 20;
 const discountStart = 10000;
 const discountCoefficient = (100 - discount) / 100;
-const productLength = Object.keys(products).length;
 const discountMessage = `Congratulations!\nWhen buying from ${discountStart} UAH you get a ${discount}%  discount!\nInstead of`;
+const productsArray = Object.keys(products);
 
-let noConvertInput = firstMessage(productLength);
+const categoryList = getCategoriesList();
 
+let customerCategoryName = productsArray[firstMessage() - 1];
+let customerCategory = products[customerCategoryName];
 
-let orderLabel = inputConvertLabel(noConvertInput);
+showProducts(customerCategory);
 
-let orderCategory = inputConvert(noConvertInput);
+let orderNumber = customerCategory[getProductNumber()];
 
-showProducts(orderCategory);
+let orderQuantity = getAmount();
 
-let productNumber = orderCategory[productNo(orderCategory, orderLabel)];
-
-let quantity = getAmount(productNumber, orderLabel);
-
-priceFunc(productNumber, quantity, discountStart, discountCoefficient, discountMessage);
+priceFunc();
