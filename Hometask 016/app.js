103d276cf4058867a24ba6e5ce7465ea36e4a3c6
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-08-24 21:27:48 +0300

    hometask 16 in progress

--- Hometask 016/app.js
+++ Hometask 016/app.js
@@ -38,4 +38,43 @@ function createCachingFunction(fn) {
   console.log(cachedAdd(4, 7));  // Вызовет add(4, 7) и вернет 11
   console.log(cachedAdd(5, 2));  // Вызовет add(5, 2) и вернет 7
   console.log(cachedAdd(2, 3));  // Из кэша вернет 5
-  
+
+
+// 'use strict'
+// const maxCashLength = 10;
+// const cacheArr = [];
+// function cacheWrapper(sumFunction, maxCashLength) {
+//     return function(...args) {
+//         let key = args.join(',');
+//         for (let value of cacheArr) {
+//             if (value.key === key) {
+//                 return value.result;
+//             }
+//         }
+//         if (cacheArr.length >= maxCashLength) {
+//             cacheArr.shift();
+//         }
+
+//         const result = sumFunction(...args);
+//         cacheArr.push({ key, result });
+//         return result;
+//     };
+// }
+
+// const sumFunction = (a, b) => {
+//     return a + b;
+// }
+
+// const cachedFunction = cacheWrapper(sumFunction,maxCashLength)
+// cachedFunction(3, 2)
+// cachedFunction(4, 2)
+// cachedFunction(5, 2)
+// cachedFunction(6, 2)
+// cachedFunction(7, 2)
+// cachedFunction(10, 2)
+// cachedFunction(11, 2)
+// cachedFunction(12, 2)
+// cachedFunction(13, 2)
+// cachedFunction(8, 2)
+// cachedFunction(9, 2)
+// console.log(cacheArr)
