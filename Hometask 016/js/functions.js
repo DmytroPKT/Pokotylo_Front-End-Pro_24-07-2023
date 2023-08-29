0a57f7a1daf7a15b8be1685fce0d9f74bf851bfa
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-08-29 12:16:37 +0300

    hometask 16 modified

---
+++ Hometask 016/js/functions.js
@@ -0,0 +1,50 @@
+'use strickt';
+
+const operationFunction = (arg1, arg2) => {
+    let out = arg1 * arg2;
+    console.log(`Output for ${arg1} & ${arg2} is ${out}`);
+    return out;
+}
+
+function cacheHandler(operFunc, cache, limit) {
+    return function (...args) {
+        let input = args.join(' & ');
+        let reverseInput = input.split('').reverse().join('');
+        for (let i of cache) {
+            if (i.input === input || i.input === reverseInput) {
+                console.log(`Cache output for ${input} is ${i.output}`);
+                return cache;
+            };
+        };
+
+        if (cache.length >= limit) {
+            cache.shift();
+        };
+
+        const output = operFunc(...args);
+        cache.push({ input, output });
+        return cache;
+    }
+}
+
+
+function getNumbers(name) {
+    let input;
+    do {
+        input = parseInt(prompt(`Enter ${name} number: `));
+    } while(isNaN(input));
+    return input;
+}
+
+function getInput(fnNumber, fnDissect) {
+    let confirmMessage = true;
+    while (confirmMessage) {
+        confirmMessage = confirm('Are you want to continue?');
+        if (!confirmMessage) {
+            break;
+        }
+        let first = fnNumber('first');
+        let second = fnNumber('second');
+        fnDissect(first, second);
+    };
+}
---
+++ Hometask 016/js/main.js
@@ -0,0 +1,57 @@
+'use strickt';
+
+let cacheList = [];
+const cacheLimit = 10;
+
+const cacheDissection = cacheHandler(operationFunction, cacheList, cacheLimit);
+
+cacheDissection(1, 2);
+cacheDissection(1, 2);
+cacheDissection(1, 2);
+cacheDissection(1, 2);
+cacheDissection(2, 2);
+cacheDissection(2, 2);
+cacheDissection(2, 2);
+cacheDissection(2, 2);
+cacheDissection(3, 2);
+cacheDissection(3, 2);
+cacheDissection(3, 2);
+cacheDissection(3, 2);
+cacheDissection(4, 2);
+cacheDissection(4, 2);
+cacheDissection(4, 2);
+cacheDissection(4, 2);
+cacheDissection(4, 2);
+cacheDissection(4, 2);
+cacheDissection(4, 2);
+cacheDissection(5, 2);
+cacheDissection(5, 2);
+cacheDissection(5, 2);
+cacheDissection(6, 2);
+cacheDissection(6, 2);
+cacheDissection(6, 2);
+cacheDissection(6, 2);
+cacheDissection(7, 2);
+cacheDissection(7, 2);
+cacheDissection(7, 2);
+cacheDissection(7, 2);
+cacheDissection(8, 2);
+cacheDissection(8, 2);
+cacheDissection(8, 2);
+cacheDissection(8, 2);
+cacheDissection(8, 2);
+cacheDissection(9, 2);
+cacheDissection(9, 2);
+cacheDissection(9, 2);
+cacheDissection(9, 2);
+cacheDissection(10, 2);
+cacheDissection(10, 2);
+cacheDissection(10, 2);
+cacheDissection(10, 2);
+cacheDissection(10, 2);
+cacheDissection(11, 2);
+cacheDissection(11, 2);
+cacheDissection(11, 2);
+cacheDissection(11, 22);
+getInput(getNumbers, cacheDissection);
+console.log(cacheList)
