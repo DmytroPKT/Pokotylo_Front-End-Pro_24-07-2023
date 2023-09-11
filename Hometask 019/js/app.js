d8e8e348ecf5df27c1a447b092c6d33d56e515c4
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-09-11 11:33:36 +0300

    ht19 complited

---
+++ Hometask 019/js/app.js
@@ -0,0 +1,67 @@
+const prodInfo = document.getElementById('right');
+const productsBlock = document.getElementById('center');
+const prodName = document.getElementById('prod-name');
+const prodPrice = document.getElementById('prod-price');
+const prodDesc = document.getElementById('prod-description');
+const buyButton = document.getElementById('buyButton');
+
+showCategories(categories);
+
+function showCategories(cat) {
+    const parentElement = document.getElementById('left');
+
+    for (let categoryKey in cat) {
+        const category = cat[categoryKey];
+
+        let element = document.createElement('div');
+        element.textContent = category.name;
+        element.setAttribute('data-category', categoryKey);
+        parentElement.appendChild(element);
+    }
+}
+
+document.getElementById('left').addEventListener('click', event => {
+    if (event.target.nodeName === 'DIV') {
+        const category = event.target.getAttribute('data-category');
+        const categoryProducts = categories[category].products;
+        showProducts(categoryProducts, category);
+    }
+});
+
+function showProducts(products, cat) {
+    const parentElement = document.getElementById('center');
+    parentElement.innerHTML = '';
+    productsBlock.style.display = 'block';
+
+    for (let product of products) {
+        let element = document.createElement('div');
+        element.textContent = `${product.name} ${product.price}$`;
+        element.setAttribute('product-id', product.id);
+        element.setAttribute('data-category', cat);
+        
+        parentElement.appendChild(element);
+    }
+}
+
+document.getElementById('center').addEventListener('click', event => {
+    if (event.target.nodeName === 'DIV') {
+        const productId = event.target.getAttribute('product-id');
+        const categoryKey = event.target.getAttribute('data-category');
+
+        const product = categories[categoryKey].products.find(product => product.id == productId );
+        showProduct(product);
+    }
+});
+
+function showProduct(prod) {
+    prodInfo.style.display = 'block';
+    prodName.textContent = prod.name;
+    prodPrice.textContent = `Price: ${prod.price}$`;
+    prodDesc.textContent = `Description: ${prod.description}`;
+}
+
+buyButton.addEventListener('click', () => {
+    alert(`You bought the product!`);
+    prodInfo.style.display = 'none';
+    productsBlock.style.display = 'none';
+});
---
+++ Hometask 019/js/data.js
@@ -0,0 +1,71 @@
+const categories = {
+    eBooks: {
+        name: 'eBooks',
+        products : [
+            {
+                id: 1,
+                name: 'Lenovo',
+                price: 340,
+                description: 'Lenovo book',
+            },
+            {
+                id: 2,
+                name: 'Pocketbook',
+                price: 240,
+                description: 'Pocketbook book',
+            },
+            {
+                id: 3,
+                name: 'Onyx',
+                price: 400,
+                description: 'Onyx book',
+            },
+        ],
+    },
+    phones: {
+        name: 'Phones',
+        products: [
+            {
+                id: 4,
+                name: 'Samsung',
+                price: 550,
+                description: 'Samsung mobile',
+            },
+            {
+                id: 5,
+                name: 'iPhone',
+                price: 600,
+                description: 'iPhone mobile',
+            },
+            {
+                id: 6,
+                name: 'OnePlus',
+                price: 400,
+                description: 'OnePlus mobile',
+            },
+        ],
+    },
+    laptops: {
+        name: 'Laptops',
+        products: [
+            {
+                id: 7,
+                name: 'Acer',
+                price: 1100,
+                description: 'Acer laptop',
+            },
+            {
+                id: 8,
+                name: 'Asus',
+                price: 1500,
+                description: 'Asus laptop',
+            },
+            {
+                id: 9,
+                name: 'Benq - Siemens',
+                price: 400,
+                description: 'Benq - Siemens laptop',
+            },
+        ],
+    }
+}
